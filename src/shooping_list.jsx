1306c355b7189074e3c39bde2895e6a2d0942c47
AmitZnati <amitznati@gmail.com>
2018-02-13 22:45:54 +0200

    shooping_list

--- src/shooping_list.jsx
+++ src/shooping_list.jsx
@@ -1,17 +1,55 @@
 import React from 'react';
+import update from 'immutability-helper';
 
 export default class ShoopingList extends React.Component{
 
     constructor(props){
         super(props);
         this.state = {
-            items: []
+            items: [],
+            imgSrc: ""
         }
     }
 
+    deleteItem = (index) =>{
+        this.setState(prevState => ({
+            items: update(prevState.items, {$splice: [[index, 1]]})
+        }))
+    }
+
+    updateBought = (count,index) => {
+        var newItems = this.state.items.slice();
+        var item = newItems[index];
+        item.qnt-= count;
+        this.setState((oldState) => ({
+            items: newItems
+        }));
+
+    }
+
+    onImgChange = (e) => {
+        var file = this.itemimg.files[0];
+        var reader = new FileReader();
+        var url = reader.readAsDataURL(file);
+        reader.onloadend = function (e) {
+            this.setState({
+                imgSrc: reader.result
+            })
+          }.bind(this);
+    }
+
     addItem = (e) => {
-        var newItem = {name: this.itemname.value, qnt: this.itemqnt.value,imgUrl: this.itemimg.value};
-        this.setState((oldState) => ({items: [...oldState.items,newItem] }));
+        console.log(this.state.counter)
+        var newItem = { 
+            name: this.itemname.value,
+            qnt: this.itemqnt.value, 
+            img_url: this.state.imgSrc 
+        }
+        this.setState((oldState) => ({items: [...oldState.items,newItem],imgSrc: ''}));
+        this.itemimg.value = '';
+        this.itemname.value = '';
+        this.itemqnt.value = '';
+        
     }
     render(){
         return (
@@ -22,7 +60,7 @@ export default class ShoopingList extends React.Component{
                 </div>
                 <div>
                     <label htmlFor="img">Item Image</label>
-                    <input ref={(itemimg) => {this.itemimg = itemimg;} } id="img" type="file"/>
+                    <input accept=".jpg,.png" ref={(itemimg) => {this.itemimg = itemimg;} } onChange={this.onImgChange} id="img" type="file"/>
                 </div>
                 <div>
                     <label htmlFor="qnt">Quantity</label>
@@ -30,16 +68,48 @@ export default class ShoopingList extends React.Component{
                 </div>
 
                 <button onClick={this.addItem}>Add</button>
-                <table style="width:100%">
-                    <tr>
-                        <th>Item Image</th>
-                        <th>Item Name</th> 
-                        <th>Quantity</th>
-                    </tr>
-                    
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Quantity</th>
+                            <th>Bought</th>
+                            <th>Bought Submit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.items.map((item,idx) =>(
+                            <ItemRow updateBought={this.updateBought} deleteItem={this.deleteItem} index={idx} key={idx} name={item.name} qnt={item.qnt} img_url={item.img_url} />
+                        ))}
+                    </tbody>
                 </table>
                 
             </div>
+        )
+    }
+}
+
+class ItemRow extends React.Component{
+
+    bought = (e) => {
+        this.props.updateBought(this.bought_count.value,this.props.index);
+        this.bought_count.value = '';
+    }
+    deleteItem = (e) =>{
+        this.props.deleteItem(this.props.index);
+    } 
+    render(){
+        return(
+            <tr>
+                <td><img style={{width: 50,height: 50}} src={this.props.img_url} /></td>
+                <td>{this.props.name}</td>
+                <td>{this.props.qnt}</td>
+                <td><input min="0" ref={(bought_count) => {this.bought_count = bought_count}} type="number"/></td>
+                <td><button onClick={this.bought}>Bought Submit</button></td>
+                <td><button onClick={this.deleteItem}>Delete</button></td>
+            </tr>
         )
     }
 }
